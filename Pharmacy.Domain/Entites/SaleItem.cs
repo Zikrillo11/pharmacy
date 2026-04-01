a49cf9d325842f3a122f85d67f1de5eb24c86cae
mirabdullayev2010 <muhammadsodiq8177@gmail.com>
2026-04-01 16:11:56 +0500

    modellar togirlandi

--- Pharmacy.Domain/Entites/SaleItem.cs
+++ Pharmacy.Domain/Entites/SaleItem.cs
@@ -1,12 +1,24 @@
-﻿namespace Pharmacy.Domain.Entites;
+﻿using System.ComponentModel.DataAnnotations.Schema;
 
+namespace Pharmacy.Domain.Entites;
+
+[Table("Saleltems")]
 public class SaleItem : BaseEntity
 {
-    public int SaleId { get; set; }
+    [Column("sale_id")]
+    public required int SaleId { get; set; }
     public Sale Sale { get; set; }
+
+    [Column("medicine_id")]
     public int MedicineId { get; set; }
     public Medicine Medicine { get; set; }
-    public int Quantity { get; set; }
+
+    [Column("quantity")]
+    public required int Quantity { get; set; }
+
+    [Column("unit_price")]
     public decimal UnitPrice { get; set; }
+
+    [Column("total_price")]
     public decimal TotalPrice { get; set; }
 }
--- Pharmacy.Domain/Entites/Supplier.cs
+++ Pharmacy.Domain/Entites/Supplier.cs
@@ -1,9 +1,18 @@
-﻿namespace Pharmacy.Domain.Entites;
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
 
+namespace Pharmacy.Domain.Entites;
+
+[Table("Suppliers")]
 public class Supplier : BaseEntity
 {
-    public string CompanyName { get; set; }
-    public string PhoneNumber { get; set; }
-    public string Address { get; set; }
+    [Column("company_name")]
+    public required string CompanyName { get; set; }
+
+    [Column("phone_number"), MaxLength(13)]
+    public required string PhoneNumber { get; set; }
+
+    [Column("address")]
+    public required string Address { get; set; }
     public ICollection<Medicine> Medicines { get; set; }
 }
